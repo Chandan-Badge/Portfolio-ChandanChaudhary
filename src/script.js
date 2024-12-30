304f7816296860883c75ee51880f42346a6b887c
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2024-12-30 10:01:41 +0530

    Portfolio Improvement 35

--- src/script.js
+++ src/script.js
@@ -25,3 +25,26 @@ let backtoTop = document.querySelector(".backtop");
 backtoTop.addEventListener("click", () => {
     window.scrollTo(0, 0);
 })
+
+// Dark & Light Button
+let darkBtn = document.querySelector(".dark-btn");
+let lightBtn = document.querySelector(".light-btn");
+let body = document.querySelector("body");
+
+lightBtn.addEventListener("click", () => {
+    darkBtn.classList.add("visible");
+    darkBtn.classList.remove("hidden");
+    lightBtn.classList.add("hidden");
+
+    body.classList.add("light");
+
+    // body.style.background = "#fff";
+})
+
+darkBtn.addEventListener("click", () => {
+    lightBtn.classList.add("visible");
+    lightBtn.classList.remove("hidden");
+    darkBtn.classList.add("hidden");
+
+    body.classList.remove("light");
+})
