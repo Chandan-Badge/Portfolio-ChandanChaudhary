91dc8fb619f3dd549e0b1b1a40fec0fa1acc1cb0
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2024-12-31 10:10:18 +0530

    Portfolio Improvement 36

--- src/script.js
+++ src/script.js
@@ -48,3 +48,53 @@ darkBtn.addEventListener("click", () => {
 
     body.classList.remove("light");
 })
+
+
+// Auto Typing
+const autoType = document.querySelector(".auto-type");
+
+let typeList = ["Java(DSA)", "Full Stack Developer", "C++", "Python"];
+
+
+function multiType(typeList, element, time) {
+
+    let listIndex = 0;
+    let charIndex = 0;
+    let skipUpdate = 0;
+    let reverseText = false;
+    
+    let intervalId = setInterval(() => {
+    
+        if(skipUpdate) {
+            skipUpdate--;
+            return;
+        }
+    
+        if(!reverseText) {
+            skipUpdate = 1;
+            element.innerText = element.innerText +  typeList[listIndex][charIndex];
+            charIndex++;
+        } else {
+            element.innerText = element.innerText.slice(0, element.innerText.length - 1);
+            charIndex--;
+        }
+    
+        if(charIndex === typeList[listIndex].length) {
+            skipUpdate = 5;
+            reverseText = true;
+        }
+    
+        if(element.innerText.length === 0 && reverseText) {
+            reverseText = false;
+            charIndex = 0;
+    
+            listIndex++;
+        }
+    
+        if(listIndex === typeList.length) {
+            listIndex = 0;
+        }
+    }, 200)
+}
+
+multiType(typeList, autoType);
