c3874b94c388bfa33c7eda2bf34f3b7ec2d1c758
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-03-26 14:45:43 +0530

    Chatbot Improvement 08

--- src/chatbot.js
+++ src/chatbot.js
@@ -1,14 +1,25 @@
 const chatBody = document.querySelector(".chat-body");
 const messageInput = document.querySelector(".message-input");
 const sendMessageBtn = document.querySelector(".send-message");
+const fileInput = document.querySelector("#file-input");
+const fileUploadWrapper = document.querySelector(".file-upload-wrapper");
+const fileCancelButton = document.querySelector("#file-cancel");
+const chatBotToggler = document.querySelector(".chatbot-toggler");
+const closeChatbot = document.querySelector(".close-chatbot");
 
 // API setup
 const API_KEY = "AIzaSyARrus7KKPz9xd5gZiln5a9wiThXg3YLKw";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
 
 const userData = {
-    message: null
+    message: null,
+    file: {
+        data: null,
+        mime_type: null
+    }
 }
+
+const chatHistory = [];
 
 // create message element with dynamic classes and return it
 const createMessageElement = (content, ...classes) => {
@@ -22,14 +33,19 @@ const createMessageElement = (content, ...classes) => {
 // Generate bot response using API
 const generateBotResponse = async(incomingMessageDiv) => {
     const messageElement = incomingMessageDiv.querySelector(".message-text");
+    
+    // Add user message to chat history
+    chatHistory.push({
+        role: "user",
+        parts: [{ text: userData.message }, ...(userData.file.data ? [{ inline_data: userData.file }] : [])]
+    })
 
+    // API request option
     const requestOptions = {
         method: "POST",
         headers: { "Content-type": "application/json" },
         body: JSON.stringify({
-            contents: [{
-                parts: [{text: userData.message}]
-            }]
+            contents: chatHistory
         })
     }
 
@@ -39,17 +55,26 @@ const generateBotResponse = async(incomingMessageDiv) => {
         const data = await response.json();
         if(!response.ok) throw new Error(data.error.message);
 
-        console.log(data);
+        // console.log(data);
 
         // Extract and display bot's response text
         const apiResponseText = data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g, "$1").trim();
         messageElement.innerText = apiResponseText;
 
+        // Add bot response to chat history
+        chatHistory.push({
+            role: "model",
+            parts: [{ text: apiResponseText }]
+        });
+
     } catch(error) {
+        // Handle error in API response
         console.log(error);
         messageElement.innerText = error.message;
         messageElement.style.color = "#ff0000";
     } finally {
+        // Reset user's file data, removing thinking indicator and scroll chat to bottom
+        userData.file = {};
         incomingMessageDiv.classList.remove("thinking");
         chatBody.scrollTo({ top: chatBody.scrollHeight, behavior: "smooth" });
     }
@@ -60,9 +85,11 @@ const handleOutgoingMessage = (e) => {
     e.preventDefault();
     userData.message = messageInput.value.trim();
     messageInput.value = "";
+    fileUploadWrapper.classList.remove("file-uploaded");
 
     // create and display user message
-    const messageContent = `<div class="message-text py-2 px-4 max-w-[75%] text-[0.95rem] text-white bg-[#5350c4] rounded-l-xl rounded-tr-xl">  </div>`;
+    const messageContent = `<div class="message-text py-2 px-4 max-w-[75%] text-[0.95rem] text-white bg-[#5350c4] rounded-l-xl rounded-tr-xl">  </div>
+                            ${userData.file.data ? `<img src="data:${userData.file.mime_type};base64,${userData.file.data}" class="attachment w-[40%] mt-1 rounded-l-lg rounded-tr-lg" />` : ""}`;
     
     const outgoingMessageDiv =  createMessageElement(messageContent, "user-message");
     outgoingMessageDiv.querySelector(".message-text").innerText = userData.message;
@@ -99,6 +126,61 @@ messageInput.addEventListener("keydown", (e) => {
     if(e.key == "Enter" && userMessage) {
         handleOutgoingMessage(e);
     }
+});
+
+// handle file input changes and preview the selected file
+fileInput.addEventListener("change", () => {
+    const file = fileInput.files[0];
+    if(!file) return;
+
+    const render = new FileReader();
+    render.onload = (e) => {
+        fileUploadWrapper.querySelector("img").src = e.target.result;
+        fileUploadWrapper.classList.add("file-uploaded");
+        const base64String = e.target.result.split(",")[1];
+
+        // Store file data in userData
+        userData.file = {
+            data: base64String,
+            mime_type: file.type
+        }
+
+        console.log(userData);
+        fileInput.value = "";
+    }
+
+    render.readAsDataURL(file);
 })
 
-sendMessageBtn.addEventListener("click", (e) => handleOutgoingMessage(e))
+// Cancel the upload
+fileCancelButton.addEventListener("click", () => {
+    userData.file = {};
+    fileUploadWrapper.classList.remove("file-uploaded");
+});
+
+// Initialize emoji picker and handle emoji section
+const picker = new EmojiMart.Picker({
+    theme: "light",
+    skinTonePosition: "none",
+    previewPosition: "none",
+    onEmojiSelect: (emoji) => {
+        const { selectionStart: start, selectionEnd: end } = messageInput;
+        messageInput.setRangeText(emoji.native, start, end, "end");
+        messageInput.focus();
+    },
+    onClickOutside: (e) => {
+        if(e.target.id === "emoji-picker") {
+            document.body.classList.toggle("show-emoji-picker");
+        } else {
+            document.body.classList.remove("show-emoji-picker");
+        }
+    }
+});
+
+document.querySelector(".chat-form").appendChild(picker);
+
+sendMessageBtn.addEventListener("click", (e) => handleOutgoingMessage(e));
+document.querySelector("#file-upload").addEventListener("click", () => fileInput.click());
+
+chatBotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
+closeChatbot.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
