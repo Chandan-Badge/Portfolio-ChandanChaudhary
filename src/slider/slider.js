64d4fe8791f32067b25ec4d41378b4dec5061d3b
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-08-12 16:51:00 +0530

    Latest work

--- src/slider/slider.js
+++ src/slider/slider.js
@@ -10,15 +10,12 @@ const swiper = new Swiper('.swiper', {
 
     pagination: {
       el: '.swiper-pagination',
-      el: '.swiper-pagination2',
       clickable: true,
     },
   
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-      nextEl: '.swiper-button-next2',
-      prevEl: '.swiper-button-prev2',
     },
     on: {
       autoplayTimeLeft(s, time, progress) {
