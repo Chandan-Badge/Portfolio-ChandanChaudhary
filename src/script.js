fa5b5113c421d5a69f40b8a8be5f58c26df9eb76
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-09-08 09:44:58 +0530

    Portfolio Improvement 140

--- src/script.js
+++ src/script.js
@@ -45,62 +45,7 @@ darkBtn.addEventListener("click", () => {
     darkBtn.classList.add("hidden");
 
     html.classList.remove("dark");
-})
-
-// Button for scroll on footer
-let aboutFoot = document.querySelector(".about-foot");
-aboutFoot.addEventListener("click", () => {
-    window.scrollTo(0, 0);
-})
-
-// let resumeFoot = document.querySelector(".resume-foot");
-// resumeFoot.addEventListener("click", () => {
-//     window.scrollTo(0, 0);
-// })
-
-let projectFoot = document.querySelector(".project-foot");
-projectFoot.addEventListener("click", () => {
-    window.scrollTo(0, 700);
-})
-
-let servicesFoot = document.querySelector(".services-foot");
-
-servicesFoot.addEventListener("click", () => {
-    window.scrollTo(0, 2950);
-})
-
-let principlesFoot = document.querySelector(".principles-foot");
-
-principlesFoot.addEventListener("click", () => {
-    window.scrollTo(0, 3450);
-})
-
-let connectFoot = document.querySelector(".connect-foot");
-
-connectFoot.addEventListener("click", () => {
-    window.scrollTo(0, 4150);
-})
-
-let testimonialsFoot = document.querySelector(".testimonials-foot");
-
-testimonialsFoot.addEventListener("click", () => {
-    window.scrollTo(0, 4800);
-})
-
-// Main Back to Top bottom
-let totopBtn = document.querySelector(".totop-btn");
-totopBtn.addEventListener("click", () => {
-    window.scrollTo(0, 0);
-})
-
-window.addEventListener("scroll", () => {
-    if(window.pageYOffset > 100) {
-        totopBtn.classList.add("visible");
-    }
-    else {
-        totopBtn.classList.remove("visible");
-    }
-})
+});
 
 // Custom Cursor
 const cursor = document.querySelector(".coursor");
